3ff509dc77514bd205ca421d655788808cc8fc54
Venice Vargas <venicevargas1@live.com>
2020-09-19 04:52:26 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,4 +6,10 @@ class EmailParser
     @csv_emails = csv_emails
   end 
   
-  
+  def parse 
+    csv_emails.split.collect do |address|
+      address.split(',') 
+    end
+    .flatten.uniq 
+  end 
+end 
