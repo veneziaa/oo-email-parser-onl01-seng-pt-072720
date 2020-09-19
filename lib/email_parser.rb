ca66cf74e78b6003691c0ac110850d23728c28f0
Venice Vargas <venicevargas1@live.com>
2020-09-19 04:52:50 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,3 +13,11 @@ class EmailParser
     .flatten.uniq 
   end 
 end 
+
+emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
+parser1 = EmailParser.new(emails1)
+parser1.parse 
+
+emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
+parser2 = EmailParser.new(emails2)
+parser2.parse
